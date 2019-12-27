ac6a02eb976ae7610e7256f859c4c670de247edb
Mikhailo Kholmohorov <mykhailo1257@gmail.com>
2019-12-27 09:59:05 +0200

    Some changed

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ server.listen(port, hostname, () => {
  console.log(`Server running at http://${hostname}:${port}/`)
 })*/
 
-
 const PORT = process.env.PORT || 3000;
 const http = require('http');
-const fs = require("fs");
+const fs = require('fs');
 //const hostname = '127.0.0.1'
 //const port = 3000
 const server = http.createServer((req, response) => {
- response.statusCode = 200;
- response.setHeader("Content-Type", "text/html");
- fs.readFile("./laba12.html", null, (error, data) => {
-  if (error) {
-    response.writeHead(404);
-    response.write("Whoops! File not found!");
-  } else {
-    response.write(data);
-  }
-  response.end();
-  });
- //res.end('Hi there!\n')
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+    fs.readFile('./laba12.html', null, (error, data) => {
+        if (error) {
+            response.writeHead(404);
+            response.write('Ooops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+    //res.end('Hi there!\n')
 });
 server.listen(PORT, () => {
- console.log(`Server running`);
+    console.log(`Server running`);
 });
